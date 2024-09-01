dade892c01bc96162cd558ffb505b1e7cc1fccdc
drivanreis <dr.ivanreis@gmail.com>
2024-09-01 13:28:30 -0300

    Iniciando Projeto

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import '../cssResetGlobal.css'
+    // Supports weights 400-700
+    import '@fontsource-variable/cabin';
+    // Supports weights 100-900
+    import '@fontsource-variable/roboto-condensed';
+    
+</script>
+
+<slot/>
